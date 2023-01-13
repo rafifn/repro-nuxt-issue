13bf769794317b62b429653e86f49b60e48c4db3
rafifn <rafiiferrynantyo@gmail.com>
2023-01-13 12:12:29 +0700

    use webpack as builder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    builder: 'webpack',
+    hooks: {
+        'webpack:config': (configs) => {
+          // configs[0] is the config for the client side
+          console.log(configs)
+          configs[0].target = "es5"
+        }
+    }
 })
